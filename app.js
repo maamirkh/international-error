3604c4db1e8bdcf1c58e4cc7c5a7a28e12a59456
maamirkh <maamirkhan1983@gmail.com>
2024-04-03 23:07:06 +0500

    updated

---
+++ app.js
@@ -0,0 +1,4 @@
+let friends = ["Ahmed", "Kashif", "Danish"];
+console.log(friends[3]); // arrays are zero indexed, index 3 out of bounds
+console.log(friends[2]); // correcting error by valid index
+export {};
---
+++ app.ts
@@ -0,0 +1,4 @@
+#! usr/bin/env node
+let friends: string[] = ["Ahmed", "Kashif", "Danish"];
+console.log(friends[3]);// arrays are zero indexed, index 3 out of bounds
+console.log(friends[2]);// correcting error by valid index
